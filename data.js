aa9dfb4c66b574f041b82628751a019f3741ed77
elazaro14 <44474164+elazaro14@users.noreply.github.com>
2026-03-05 19:16:03 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+export const subjectLists = {
+    "Form 1": [
+        { name: "Historia na Maadi", code: "HMT" },
+        { name: "History", code: "HIST" },
+        { name: "Geography", code: "GEO" },
+        { name: "Kiswahili", code: "KISW" },
+        { name: "English Language", code: "ENG" },
+        { name: "Physics", code: "PHY" },
+        { name: "Chemistry", code: "CHEM" },
+        { name: "Biology", code: "BIO" },
+        { name: "Mathematics", code: "MATH" },
+        { name: "Business Studies", code: "BUS" }
+    ],
+    "Form 2": [ /* Identical to Form 1 */ ],
+    "Form 3": [
+        { name: "Civics", code: "CIV" },
+        { name: "History", code: "HIST" },
+        { name: "Geography", code: "GEO" }
+        // ... additional subjects
+    ]
+};
